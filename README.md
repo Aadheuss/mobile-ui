c9144a0c1583295cdb6a6472c09fda59cb2d08a0
Aadheuss <aadheusx@gmail.com>
2023-10-05 17:10:26 +0700

    Add mbile tab ui functionality

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # mobile-ui
+
+## How to install and use aadheuss-mobile-ui
+
+`npm install aadheuss-mobile-ui`
+
+### on you html file:
+
+- Add "main-nav" as a main container.
+- Add either "top" or "bottom" to initiate main container position on the screen.
+- Make a child with a class of "main-nav-container".
+- Create items inside "main-nav-container" with a class of "main-nav-item".
+
+Example:
+
+```
+  <nav class="main-nav bottom">
+    <div class="main-nav-container">
+      <button class="main-nav-item">item-1</button>
+      <button class="main-nav-item">item-2</button>
+      <button class="main-nav-item">item-3</button>
+      <button class="main-nav-item">item-4</button>
+      <button class="main-nav-item">item-5</button>
+    </div>
+  </nav>
+```
+
+### on your javascript file
+
+```
+import activateAllNavBar from 'aadheuss-mobile-ui/mobile-ui'.
+import 'aadheuss-mobile-ui/mobile-ui.css'.
+use activateAllNavBar on you file.
+```
---
+++ mobile-ui.js
@@ -0,0 +1,79 @@
+function activateAllNavBar() {
+  const navBar = document.querySelectorAll(".main-nav");
+  activateNavItems(navBar);
+}
+
+function activateNavItems(nav) {
+  if (!isNodeListEmpty(nav)) {
+    nav.forEach((item) => activateNavBar(item));
+  }
+}
+
+function activateNavBar(nav) {
+  const navItems = nav.querySelectorAll(".main-nav-item");
+  const selectedItem = nav.querySelector(".main-nav-item");
+  selectedItem.classList.add("active");
+
+  if (!isNodeListEmpty(navItems)) {
+    navItems.forEach((item) => activateNavItem(item));
+  }
+}
+
+function activateNavItem(item) {
+  item.addEventListener("click", itemSelected.bind(this, item));
+  item.addEventListener("mouseenter", activateHover);
+  item.addEventListener("mouseleave", deactivateHover);
+}
+
+function itemSelected(item) {
+  resetActive(item, "main-nav-container", "main-nav-item", "active");
+  item.classList.add("active");
+}
+
+function resetActive(item, parentClass, siblingClass, classToRemove) {
+  const itemContainer = findAncestorByClass(item, parentClass);
+  const els = itemContainer.querySelectorAll(`.${siblingClass}`);
+  els.forEach((el) => el.classList.remove(classToRemove));
+}
+
+// Find the ancestor of an element by it class name
+function findAncestorByClass(el, clsName) {
+  if (doesElContainClass(el, clsName)) {
+    return el;
+  } else {
+    if (!isBodyEl(el)) {
+      return findAncestorByClass(el.parentElement, clsName);
+    }
+  }
+}
+
+// Check if an element contain the specified class name
+function doesElContainClass(el, clsName) {
+  return Array.from(el.classList).includes(clsName);
+}
+
+function isBodyEl(el) {
+  return el === document.querySelector("body");
+}
+
+function isNodeListEmpty(node) {
+  return node.length === 0;
+}
+
+// Hover functions
+
+function activateHover(e) {
+  setTimeout(removeOnHover.bind(this, e.target), 900);
+}
+
+function deactivateHover(e) {
+  e.target.classList.remove("on-hover");
+}
+
+function removeOnHover(target) {
+  if (target.matches(":hover") && !doesElContainClass(target, "active")) {
+    target.classList.add("on-hover");
+  }
+}
+
+export { activateAllNavBar };
